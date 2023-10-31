43ba4517b8e6608786b65d5af2ff73519a89ff7d
Ronald Martin <ronaldmartin02@gmail.com>
2023-10-30 21:47:59 -0400

    added styling and added facts div

--- Js/icebreakers.js
+++ Js/icebreakers.js
@@ -53,13 +53,13 @@ function FactsGen(){
     console.error('Error: ', error);
   });
 }
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+// var slider = document.getElementById("myRange");
+// var output = document.getElementById("demo");
+// output.innerHTML = slider.value; // Display the default slider value
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
+// // Update the current slider value (each time you drag the slider handle)
+// slider.oninput = function() {
+//   output.innerHTML = this.value;
+// }
 
 triviaBtn.addEventListener("click", triviaGen)
